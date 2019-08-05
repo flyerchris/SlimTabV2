965d8d1d846dbadf060ded3df0878cccfbb16659
Chris <flyernaruto@gmail.com>
2019-08-06 05:05:52 +0800

    add demo code

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/SlimTabV2.ts',
+    entry: {
+        "demo": "./src/demo.ts",
+    },
     devtool: 'inline-source-map',
     module: {
         rules: [
@@ -16,7 +18,7 @@ module.exports = {
         extensions: [ '.tsx', '.ts', '.js' ]
     },
     output: {
-        filename: 'SlimTabV2.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     }
 };
