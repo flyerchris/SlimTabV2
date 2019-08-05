fed2ec5f21e0bf770540e300d7f6c66e1921121b
Chris <flyernaruto@gmail.com>
2019-08-05 19:08:43 +0800

    draw note from data

--- src/SlimTabV2.ts
+++ src/SlimTabV2.ts
@@ -3,22 +3,25 @@ import {utils} from "./utils"
 export class SLTab {
     private lengthPerBeat: number = 4;
     private beatPerSection: number = 4;
-    private setionWidth: number = 200; // 單位是 pixel
+    private sectionWidth: number = 260; // 單位是 pixel
     private sectionPerLine: number = 4;
-    private lineAdditionLength: number = 30; // 每行第一小節前方會有文字，所以要給多一點空間
-    private linePadding: number = 16;
+    private lineAdditionLength: number = 50; // 每行第一小節前方會有文字，所以要給多一點空間
+    private stringPadding: number = 16;
     private linePerPage: number = 20;
     private notes: [number, number[], any][][];
     private noteElement: SVGElement[] = [];
     private svgElement: SVGElement;
+    private startPosition: number[] = [90, 20]; // x, y
     private lineInfo: [number, number, number] = [0, 20, 20]; // 總行數, 最後一行的 X, 最後一行的 Y
 
-    constructor(data?: {lengthPerBeat?: number, beatPerSection?: number, setionWidth?: number, sectionPerLine?: number, linePerPage?: number}) {
+    constructor(data?: {lengthPerBeat?: number, beatPerSection?: number, sectionWidth?: number, sectionPerLine?: number, linePerPage?: number}) {
         Object.assign(this, data);
         this.notes = [
             [// 小節
-                [4, [-1, -1, -1, -1, -1, -1], null],// 音符長度, [格數，索引值為弦的編號，從零開始算], user data
-                [4, [-1, -1, -1, -1, -1, -1], null],
+                [4, [3, -1, -1, 4, -1, -1], null],// 音符長度, [格數，索引值為弦的編號，從零開始算], user data
+                [4, [-1, 5, 2, -1, -1, -1], null],
+                [4, [-1, 5, 2, -1, -1, -1], null],
+                [4, [-1, 5, 2, -1, -1, -1], null]
             ],
             [
                 [4, [-1, -1, -1, -1, -1, -1], null],
@@ -61,7 +64,7 @@ export class SLTab {
         this.svgElement = document.createElementNS('http://www.w3.org/2000/svg',"svg");
         utils.setAttributes(this.svgElement,{width: "1200", height: "600"});
         this.drawAllLine();
-        this.createNoteElement();
+        this.setNoteElementData(this.calNoteRawData());
         page.append(this.svgElement);
         document.body.appendChild(page);
     }
@@ -77,9 +80,9 @@ export class SLTab {
     }
     private drawLine(x: number, y: number) {
         for(let i = 0; i < 6; i++){
-            let l = this.sectionPerLine*this.setionWidth + this.lineAdditionLength;
+            let l = this.sectionPerLine*this.sectionWidth + this.lineAdditionLength;
             let newLine = document.createElementNS('http://www.w3.org/2000/svg',"line");
-            utils.setAttributes(newLine,{x1: `${x}`, y1: `${y + i*this.linePadding}`, x2: `${x + l}`, y2: `${y + i*this.linePadding}`, style: "stroke:black;stroke-width:1"});
+            utils.setAttributes(newLine,{x1: `${x}`, y1: `${y + i*this.stringPadding}`, x2: `${x + l}`, y2: `${y + i*this.stringPadding}`, style: "stroke:black;stroke-width:1"});
             this.svgElement.appendChild(newLine);
         }
         this.drawLineTitle(x,y);
@@ -87,7 +90,7 @@ export class SLTab {
         this.drawBar(x , y);
         x += this.lineAdditionLength;
         for(let i = 1; i < this.sectionPerLine + 1; i++){
-            this.drawBar(x + this.setionWidth*i, y);
+            this.drawBar(x + this.sectionWidth*i, y);
         }
     }
     private drawLineTitle(x: number, y: number){
@@ -105,25 +108,65 @@ export class SLTab {
     private drawBar(x: number, y: number) {
         let bar = document.createElementNS('http://www.w3.org/2000/svg',"line");
         utils.setAttributes(bar,{style: 'stroke:black;stroke-width:1'})
-        utils.setAttributes(bar,{x1: `${x}`, y1: `${y}`, x2: `${x}`, y2: `${y + this.linePadding * 5}`})
+        utils.setAttributes(bar,{x1: `${x}`, y1: `${y}`, x2: `${x}`, y2: `${y + this.stringPadding * 5}`})
         this.svgElement.appendChild(bar);
     }
 
-    private createNoteElement(){
+    private createNoteElement(x: number = 200, y:number = 20){
         let note = document.createElementNS('http://www.w3.org/2000/svg',"g");
-        let noteHtml = "";
-        let x = 200, y = 20;
+        let noteHtml = `<g>
+        <line style="stroke:black;stroke-width:2" x1="${x}" y1="140" x2="${x}" y2="${y}"></line>
+        <line style="stroke:black;stroke-width:4" x1="${x}" y1="138" x2="${x+10}" y2="138"></line>
+        <line style="stroke:black;stroke-width:2" x1="${x}" y1="130" x2="${x+10}" y2="130"></line>
+        </g>`;
         for(let i = 0; i < 6 ; i++){
             noteHtml += `
                 <g> 
-                <circle cx='${x}' cy='${y + this.linePadding * i}' r='6' fill='white' stroke-width='0' stroke='black' style='cursor:pointer;'></circle>
-                <text x='${x }' y='${y + this.linePadding * i + 4}' text-anchor="middle" style="font:bold 12px Sans-serif">3</text>
+                <circle cx='${x}' cy='${y + this.stringPadding * i}' r='6' fill='#fff' stroke-width='0' stroke='black' style='cursor:pointer;'></circle>
+                <text x='${x }' y='${y + this.stringPadding * i + 4}' text-anchor="middle" style="font:bold 12px Sans-serif">3</text>
                 </g>
             `;
         }
         note.innerHTML = noteHtml;
         this.svgElement.appendChild(note);
         this.noteElement.push(note);
+    }
+
+    private calNoteRawData(): [number, number, number, number[]][]{ // x, y , length, block of every chord
+        let [x, y] = this.startPosition;
+        let beatLength = (this.sectionWidth - 20) / this.beatPerSection;
+        let rawData: [number, number, number, number[]][] = [];
+        for(let s = 0; s < this.notes.length; s++){ // section
+            if(s % this.sectionPerLine == 0)x = this.startPosition[0];
+            let nx = x + this.sectionWidth;
+            for(let i = 0; i < this.notes[s].length; i++){ // note
+                let note = this.notes[s][i];
+                rawData.push([x, y, note[0], note[1]]);
+                let step = beatLength * this.lengthPerBeat / note[0];
+                x += step;
+            }
+            x = nx;
+        }
+        return rawData;
+    }
+    private setNoteElementData(data: [number, number, number, number[]][]){
+        let ne = data.length - this.noteElement.length;
+        for(let i = 0; i < ne; i++){
+            this.createNoteElement();
+        }
+        for(let i = 0; i < data.length; i++){
+            this.setElementPosition(this.noteElement[i], data[i][0], data[i][1]);
+            //this.setChordVisiable(this.noteElement, data[i][3]);
+        }
+    }
+    private setElementPosition(e:SVGElement, x:number, y:number){
+        utils.setAttributes(e.children[0].children[0],{x1: `${x}`, y1: `${40 + y + this.stringPadding * 5}`, x2: `${x}`, y2: `${y}`});
+        utils.setAttributes(e.children[0].children[1],{x1: `${x}`, y1: `${38 + y + this.stringPadding * 5}`, x2: `${x + 10}`, y2: `${38 + y + this.stringPadding * 5}`});
+        utils.setAttributes(e.children[0].children[2],{x1: `${x}`, y1: `${30 + y + this.stringPadding * 5}`, x2: `${x + 10}`, y2: `${30 + y + this.stringPadding * 5}`});
+        for(let i = 1 ; i <= 6; i++){
+            utils.setAttributes(e.children[i].children[0], {cx: `${x}`, cy: `${y + this.stringPadding * (i-1)}`});
+            utils.setAttributes(e.children[i].children[1], {x: `${x}`, y: `${y + this.stringPadding * (i-1) + 4}`});
+        }
     }
 }
 let n = new SLTab();
