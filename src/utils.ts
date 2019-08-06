6ca651e2773d5d89e1c4257d6ca5eba0e786e585
tim37021 <tim37021@gmail.com>
2019-08-06 13:21:06 +0800

    Added Callbacks class

--- src/utils.ts
+++ src/utils.ts
@@ -11,4 +11,26 @@ let utils = {
     }
 }
 
+class ArrayFunction {
+    private funcs: Array<Function> = [];
+    call(...args: any[]) {
+        this.funcs.forEach((func) => {
+            func(...args);
+        })
+    }
+
+    push(cbk: (...args: any[]) => void): void {
+        this.funcs.push(cbk);
+    }
+};
+
+export class Callbacks {
+    [key: string]: ArrayFunction;
+    constructor(eventType: Array<string>) {
+        eventType.forEach(element => {
+            this[element] = new ArrayFunction();
+        });
+    }
+}
+
 export {utils};
