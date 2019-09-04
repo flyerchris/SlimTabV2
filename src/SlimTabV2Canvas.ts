39d85f3dcf2a4e6e572624c201bc662c3019ee3d
ps2747 <ps2747@gmail.com>
2019-09-04 20:57:21 +0800

    Wrapped up some SVG element to SVGShape.

--- src/SlimTabV2Canvas.ts
+++ src/SlimTabV2Canvas.ts
@@ -3,10 +3,10 @@ import { svgNote } from './SlimTabV2Types';
 
 interface SVGPrimitiveRenderer {
     clear(): void;
-    createRect(x: number, y: number, width: number, height: number, strokeWidth: number, color: string): SVGRectElement;
-    createEllipse(cx: number, cy: number, rx: number, ry: number): SVGEllipseElement;
-    createText(x: number, y: number, str: string, anchor: string): SVGTextElement;
-    createLine(x1: number, y1: number, x2: number, y2: number, width: number, color?: string): SVGElement;
+    createRect(x: number, y: number, width: number, height: number, strokeWidth: number, color: string): Rect;
+    createEllipse(cx: number, cy: number, rx: number, ry: number): Ellipse;
+    createText(x: number, y: number, str: string, anchor: string): Text;
+    createLine(x1: number, y1: number, x2: number, y2: number, width: number, color?: string): Line;
 }
 
 export class Layer implements SVGPrimitiveRenderer {
@@ -20,7 +20,7 @@ export class Layer implements SVGPrimitiveRenderer {
         this.domElement.innerHTML = "";
     }
 
-    createRect(x: number, y: number, width: number, height: number, strokeWidth: number, fill: string, target?: SVGElement): SVGRectElement {
+    createRect(x: number, y: number, width: number, height: number, strokeWidth: number, fill: string, target?: SVGElement): Rect {
         const inst = document.createElementNS('http://www.w3.org/2000/svg',"rect");
         utils.setAttributes(inst, {
             x: `${x}`, y:`${y}`,
@@ -32,10 +32,10 @@ export class Layer implements SVGPrimitiveRenderer {
             this.domElement.appendChild(inst);
         else
             target.appendChild(inst);
-        return inst;
+        return new Rect(inst);
     }
 
-    createEllipse(cx: number, cy: number, rx: number, ry: number, target?: SVGElement): SVGEllipseElement {
+    createEllipse(cx: number, cy: number, rx: number, ry: number, target?: SVGElement): Ellipse {
         const inst = document.createElementNS('http://www.w3.org/2000/svg',"ellipse");
         utils.setAttributes(inst, {
             cx: `${cx}`, cy: `${cy}`,
@@ -46,10 +46,10 @@ export class Layer implements SVGPrimitiveRenderer {
             this.domElement.appendChild(inst);
         else
             target.appendChild(inst);
-        return inst;
+        return new Ellipse(inst);
     }
 
-    createText(x: number, y: number, str: string, anchor: string, target?: SVGElement): SVGTextElement {
+    createText(x: number, y: number, str: string, anchor: string, target?: SVGElement): Text {
         const inst = document.createElementNS('http://www.w3.org/2000/svg',"text");
         utils.setAttributes(inst, {
             x: `${x}`, y: `${y}`,
@@ -62,9 +62,9 @@ export class Layer implements SVGPrimitiveRenderer {
             this.domElement.appendChild(inst);
         else
             target.appendChild(inst);
-        return inst;
+        return new Text(inst);
     }
-    createLine(x1: number, y1: number, x2: number, y2: number, width: number, color: string, target?: SVGElement): SVGElement {
+    createLine(x1: number, y1: number, x2: number, y2: number, width: number, color: string, target?: SVGElement): Line {
         const inst = document.createElementNS('http://www.w3.org/2000/svg',"line");
         utils.setAttributes(inst, {
             x1: `${x1}`, y1: `${y1}`,
@@ -77,7 +77,7 @@ export class Layer implements SVGPrimitiveRenderer {
             this.domElement.appendChild(inst);
         else
             target.appendChild(inst);
-        return inst;
+        return new Line(inst);
     };
     createLinker(target?: SVGElement){
         let linker = document.createElementNS('http://www.w3.org/2000/svg',"path");
@@ -89,6 +89,242 @@ export class Layer implements SVGPrimitiveRenderer {
         utils.setAttributes(linker, {fill: `white`});
         this.linker.push(linker);
         return linker;
+    }
+}
+
+
+class SVGShape {
+    protected _domElement: SVGElement;
+    constructor(domElement: SVGElement) {
+        this._domElement = domElement;
+    }
+    remove() {
+        this._domElement.remove();
+    }
+    get domElement() {
+        return this._domElement;
+    }
+    set style(val: string){
+        utils.setAttributes(this.domElement, {
+            "style": `${val}`
+        });
+    }
+    get style(): string{
+        return this.domElement.getAttribute("style");
+    }
+};
+
+class Rect extends SVGShape{
+    constructor(domElement: SVGRectElement) {
+        super(domElement);
+    }
+    
+    set x(val: number) {
+        utils.setAttributes(this.domElement, {
+            x: `${val}`
+        });
+    }
+
+    get x(): number {
+        return Number(this.domElement.getAttribute("x"));
+    }
+
+    set y(val: number){
+        utils.setAttributes(this.domElement, {
+            y: `${val}`
+        });
+    }
+
+    get y(): number{
+        return Number(this.domElement.getAttribute("y"));
+    }
+
+    set width(val: number){
+        utils.setAttributes(this.domElement, {
+            width: `${val}`
+        });
+    }
+
+    get width(): number{
+        return Number(this.domElement.getAttribute("width"));
+    }
+    
+    set height(val: number){
+        utils.setAttributes(this.domElement, {
+            height: `${val}`
+        });
+    }
+
+    get height(): number{
+        return Number(this.domElement.getAttribute("height"));
+    }
+
+    set strokeWidth(val: number){
+        utils.setAttributes(this.domElement, {
+            "stroke-width": `${val}`
+        });
+    }
+
+    get strokeWidth(): number{
+        return Number(this.domElement.getAttribute("stroke-width"));
+    }
+
+    set fill(val: string){
+        utils.setAttributes(this.domElement, {
+            fill: `${val}`
+        });
+    }
+    
+    get fill(): string{
+        return this.domElement.getAttribute("fill");
+    }
+}
+
+class Ellipse extends SVGShape{
+    constructor(domElement: SVGEllipseElement) {
+        super(domElement);
+    }
+
+    set cx(val: number){
+        utils.setAttributes(this.domElement, {
+            cx: `${val}`
+        });
+    }
+    
+    get cx():number{
+        return Number(this.domElement.getAttribute("cx"));
+    }
+
+    set cy(val: number){
+        utils.setAttributes(this.domElement, {
+            cy: `${val}`
+        });
+    }
+
+    get cy(): number{
+        return Number(this.domElement.getAttribute("cy"));
+    }
+
+    set rx(val: number){
+        utils.setAttributes(this.domElement, {
+            rx: `${val}`
+        });
+    }
+
+    get rx(): number{
+        return Number(this.domElement.getAttribute("rx"));
+    }
+
+    set ry(val: number){
+        utils.setAttributes(this.domElement, {
+            ry: `${val}`
+        });
+    }
+
+    get ry(): number{
+        return Number(this.domElement.getAttribute("ry"));
+    }
+}
+
+class Line extends SVGShape{
+    constructor(domElement: SVGElement) {
+        super(domElement);
+    }
+    set x1(val: number){
+        utils.setAttributes(this.domElement, {
+            x1: `${val}`
+        });
+    }
+    get x1(): number{
+        return Number(this.domElement.getAttribute("x1"));
+    }
+    set y1(val: number){
+        utils.setAttributes(this.domElement, {
+            y1: `${val}`
+        });
+    }
+    get y1(): number{
+        return Number(this.domElement.getAttribute("y1"));
+    }
+    set x2(val: number){
+        utils.setAttributes(this.domElement, {
+            x2: `${val}`
+        });
+    }
+    get x2(): number{
+        return Number(this.domElement.getAttribute("x2"));
+    }
+    set y2(val: number){
+        utils.setAttributes(this.domElement, {
+            y2: `${val}`
+        });
+    }
+    get y2(): number{
+        return Number(this.domElement.getAttribute("y2"));
+    }
+    set stroke(val: string){
+        utils.setAttributes(this.domElement, {
+            stroke: `${val}`
+        });
+    }
+    get stroke(): string{
+        return this.domElement.getAttribute("stroke")
+    }
+    set strokeWidth(val: number){
+        utils.setAttributes(this.domElement, {
+            "stroke-width": `${val}`
+        });
+    }
+    get strokeWidth(): number{
+        return Number(this.domElement.getAttribute("stroke-width"))
+    }
+}
+
+class Text extends SVGShape{
+    constructor(domElement: SVGTextElement) {
+        super(domElement);
+    }
+    set x(val: number){
+        utils.setAttributes(this.domElement, {
+            x: `${val}`
+        });
+    }
+    get x(): number{
+        return Number(this.domElement.getAttribute("x"));
+    }
+    set y(val: number){
+        utils.setAttributes(this.domElement, {
+            y: `${val}`
+        });
+    }
+    get y(): number{
+        return Number(this.domElement.getAttribute("y"));
+    }
+    set anchor(val: string){
+        utils.setAttributes(this.domElement, {
+            "text-anchor": `${val}`
+        });
+    }
+    get anchor(): string{
+        return this.domElement.getAttribute("text-anchor");
+    }
+
+    set text(val: string){
+        this.domElement.innerHTML = `${val}`;
+    }
+
+    get text(): string{
+        return this.domElement.innerHTML;
+    }
+
+    set strokeWidth(val: number){
+        utils.setAttributes(this.domElement, {
+            "stroke-width": `${val}`
+        });
+    }
+
+    get strokeWidth(): number{
+        return Number(this.domElement.getAttribute("stroke-width"));
     }
 }
 
@@ -113,7 +349,9 @@ class NoteLayer extends Layer {
             const wordBack = this.createText(0, 0, "", "middle", wordGroup);
             const word = this.createText(0, 0, "", "middle", wordGroup);
             utils.setAttributes(wordGroup, {"stroke-width": '0', "stroke": "black", style: "cursor:pointer;"});
-            utils.setAttributes(wordBack, {"stroke-width": "3", style: "font: 12px Sans-serif"});
+            wordBack.strokeWidth = 3; 
+            wordBack.style = "font: 12px Sans-serif"
+            //utils.setAttributes(wordBack, {"stroke-width": "3", style: "font: 12px Sans-serif"});
             utils.setStyle(<HTMLElement><unknown>word, {"font": "12px Sans-serif", "fill": "#fff"});
             blockGroup.append(wordGroup);
             blockArray.push({groupElement: <HTMLElement><unknown>wordGroup, word: <HTMLElement><unknown>word, wordBack: <HTMLElement><unknown>wordBack});
@@ -169,14 +407,15 @@ class SheetLayer extends Layer {
 }
 
 class UILayer extends Layer {
-    readonly sectionIndicator: SVGElement[] = [];
+    readonly sectionIndicator: Rect[] = [];
     constructor(){
         super();
         utils.setAttributes(this.domElement,{"data-layer": "UILayer"})
     }
     createSectionIndicator(){
         let newSquare = this.createRect(0, 0, 0, 0, 0,"rgba(0, 255, 255, 0.13)");
-        utils.setAttributes(newSquare, {style: "display: none"});
+        newSquare.style = "display: none";
+        //utils.setAttributes(newSquare, {style: "display: none"});
         this.sectionIndicator.push(newSquare);
         return newSquare;
     }
