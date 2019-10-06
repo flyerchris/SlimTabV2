8e15ea3614e6cfa0d1885ef962c077b0b0ebf9c9
tim37021 <tim37021@gmail.com>
2019-10-06 17:13:40 +0800

    adjust adapter percision

--- src/DataAdapter.ts
+++ src/DataAdapter.ts
@@ -8,6 +8,7 @@ export class DataAdapter{
     private spb: number = 60 * 1000 / 120; // second per beat, defalut value corresponds to 120 bpm
     private lengthPerBeat: number = 4;
     private callbacks: Callbacks;
+    private chordInterval: number = 150;
     
     constructor(bpm?: number){
         if(bpm){
@@ -18,7 +19,7 @@ export class DataAdapter{
     receiveData(stringIndex: number, note: number, amp: number, time: number){
         this.rawData.push([stringIndex, note, time]);
         if(!this.receiveInterval){
-            this.receiveInterval = setTimeout(this.packNote.bind(this),20);
+            this.receiveInterval = setTimeout(this.packNote.bind(this), this.chordInterval);
         }
         this.callbacks["data"].callAll(stringIndex, note, time);
     }
@@ -44,9 +45,8 @@ export class DataAdapter{
                 let l2 = Math.log(this.spb / duration) / Math.log(2);
                 let l3 = Math.log(this.spb / (3 * duration)) / Math.log(2) + 1;
                 let bl = Math.pow(2, Math.ceil(l2 - 0.5));
-                this.noteRawData[0] = this.lengthPerBeat * bl;
+                this.noteRawData[0] = Math.min(this.lengthPerBeat * bl, 32);
             }
-
             this.callbacks["packNote"].callAll(this.noteRawData);
 
         }
