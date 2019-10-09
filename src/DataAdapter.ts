80ba7ee2d591f81762bb947bbeb6f6400bccefb2
Chris <flyernaruto@gmail.com>
2019-10-09 17:56:44 +0800

    count note value according to timeline

--- src/DataAdapter.ts
+++ src/DataAdapter.ts
@@ -10,7 +10,8 @@ export class DataAdapter{
     private lengthPerBeat: number = 4;
     private callbacks: Callbacks;
     private timeline: Timeline;
-    private chordInterval: number = 150;
+    private chordInterval: number = 60;
+    private receiveTime: number = 0;
 
     constructor(tl: Timeline, bpm?: number){
         if(bpm){
@@ -22,6 +23,7 @@ export class DataAdapter{
     receiveData(stringIndex: number, note: number, amp: number, time: number){
         this.rawData.push([stringIndex, note, time]);
         if(!this.receiveInterval){
+            this.receiveTime = this.timeline.getElapseTime();
             this.receiveInterval = setTimeout(this.packNote.bind(this), this.chordInterval);
         }
         this.callbacks["data"].callAll(stringIndex, note, time);
@@ -35,7 +37,7 @@ export class DataAdapter{
     getTime(): number{
         return performance.now();
     }
-    get spb(): number{
+    get milliSecondPerBeat(): number{
         return 60 * 1000 / this.bpm;
     }
     private packNote(){
@@ -43,23 +45,23 @@ export class DataAdapter{
         for(let i = 0; i < this.rawData.length; i++){
             notes[this.rawData[i][0]] = this.rawData[i][1];
         }
-        if(this.noteRawData){
-            let duration = this.timeline.getElapseTime() - this.preTime;
-            if(duration > this.spb * this.lengthPerBeat){
-                this.noteRawData[0] = 1;
-            }else{
-                let l2 = Math.log(this.spb / duration) / Math.log(2);
-                let l3 = Math.log(this.spb / (3 * duration)) / Math.log(2) + 1;
-                let bl = Math.pow(2, Math.ceil(l2 - 0.5));
-                this.noteRawData[0] = Math.min(this.lengthPerBeat * bl, 32);
-            }
+        if(this.noteRawData){   
+            let nv = this.lengthPerBeat / (this.timeToBeatCount(this.receiveTime) - this.timeToBeatCount(this.preTime));
+            this.noteRawData.noteValue = nv;
             this.noteRawData.userData = null;
             this.callbacks["packNote"].callAll(this.noteRawData);
         }
-        this.preTime = this.rawData[0][2];
         this.noteRawData = new Note({noteValue: 8, stringContent: notes, userData: "undefined-value"});
         this.rawData = [];
         this.receiveInterval = null;
         this.callbacks["packNote"].callAll(this.noteRawData);
+        this.preTime = this.receiveTime;
+    }
+    private timeToBeatCount(time: number){
+        let beat = time / this.milliSecondPerBeat;
+        let bd = Math.floor(beat);
+        let bc = beat - bd;
+        bc = Math.floor(bc * 4 + 0.5) / 4;
+        return bd + bc;
     }
 }
