93eab5feedd04de2c61b3d8689c76ffc45bc24aa
Chris <flyernaruto@gmail.com>
2019-10-09 17:54:58 +0800

    implement timeline

--- src/Timeline.ts
+++ src/Timeline.ts
@@ -1,36 +1,22 @@
 export class Timeline{
-    private startTime: number;
-    private bpm: number = 120;
-    private offset: number = 0;
+    private _startTime: number;
+    offset: number = 0;
 
-    constructor(bpm?: number){
-        this.startTime = this.ajustedTime();
-        if(bpm) this.bpm = bpm;
+    constructor(){
+        this._startTime = performance.now();
     }
 
-    reset(){
-        this.offset = 0;
-        this.startTime = this.ajustedTime();
+    reset(offset: number = 0){
+        this.offset = offset;
+        this._startTime = performance.now();
     }
 
     getElapseTime(): number{
         return this.ajustedTime() - this.startTime;
     }
 
-    getBeatCount(): number{
-        let beat = this.getElapseTime() / this.secondPerBeat;
-        let bd = Math.floor(beat);
-        let bc = beat - bd;
-        bc = Math.floor(bc*32 + 0.5) / 32;
-        return bd + bc;
-    }
-
-    get secondPerBeat(): number{
-        return 60 * 1000 / this.bpm;
-    }
-    
-    setOffset(val: number){
-        this.offset = val;
+    get startTime(): number{
+        return this._startTime;
     }
 
     private ajustedTime(): number{
